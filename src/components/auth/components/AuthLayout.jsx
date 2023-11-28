eeb7fbc32706ceed9e971107c80affedd98cd8be
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-28 08:10:07 +0700

    simple layouting

---
+++ src/components/auth/components/AuthLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AuthLayout = ({children}) => {
+  return (
+    <main>
+        <div className=' bg-rose-300 m-40 p-48'>{children}</div>
+    </main>
+  )
+}
